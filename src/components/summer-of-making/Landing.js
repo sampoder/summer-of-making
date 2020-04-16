ebea3a1944d2a8ff631e399b58f48147e0bfba1b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-04-16 11:14:14 +0800

    Update Landing.js

--- src/components/summer-of-making/Landing.js
+++ src/components/summer-of-making/Landing.js
@@ -91,7 +91,7 @@ export default () => (
     >
       <Fade bottom>
         <Heading.h1 fontSize={[6, 7, 8, 9]}>
-          Make something <Underline>awesome</Underline>this{' '}
+          Make something <Underline>awesome</Underline> this{' '}
           <Underline>summer</Underline>.
         </Heading.h1>
         <Lead maxWidth={48} my={4} fontSize={[3, 4]}>
